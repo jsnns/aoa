8bb755da9d9c1a585138b3754f74ebe2cce0dbf6
Jacob Sansbury <sansburyjacob@gmail.com>
2024-02-13 16:51:43 -0800

    only show chart on desktop

--- src/app/Phase.tsx
+++ src/app/Phase.tsx
@@ -26,7 +26,7 @@ export const Phase: React.FC<Props> = async ({ phase }) => {
 
       <Link href={`/phase/${phase.slug}`}>
         <Card className="py-3 px-5 md:hover:absolute bottom-0 left-0 right-0 group flex flex-col gap-5 shrink-0">
-          <div className="group-hover:flex hidden max-w-full">
+          <div className="md:group-hover:flex hidden max-w-full">
             <PhasePredictionChart
               phase={phase}
               className="h-24 w-full overflow-x-auto"
