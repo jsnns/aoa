380ef17e5865df2695f01ecc92310ed4a8f60aba
Jacob Sansbury <sansburyjacob@gmail.com>
2024-02-13 17:29:39 -0800

    Full details toggle

--- src/app/PhaseColumn.tsx
+++ src/app/PhaseColumn.tsx
@@ -2,9 +2,10 @@ import { Phase } from "@/data/phases";
 
 interface Props {
   phase: Phase;
+  fullDetails?: boolean;
 }
 
-export const PhaseColumn: React.FC<Props> = ({ phase }) => {
+export const PhaseColumn: React.FC<Props> = ({ phase, fullDetails }) => {
   const headingStyle = "text-sm opacity-50 mt-3";
 
   return (
@@ -12,23 +13,27 @@ export const PhaseColumn: React.FC<Props> = ({ phase }) => {
       <h2 className="text-xl font-semibold">{phase.title}</h2>
       <p className="">{phase.summary}</p>
 
-      {/* <h3 className={headingStyle}>How it starts</h3>
-      <p>{phase.how_it_starts}</p>
+      {fullDetails && (
+        <>
+          <h3 className={headingStyle}>How it starts</h3>
+          <p>{phase.how_it_starts}</p>
 
-      <h3 className={headingStyle}>Limitations</h3>
-      <p>{phase.limitations}</p>
+          <h3 className={headingStyle}>Limitations</h3>
+          <p>{phase.limitations}</p>
 
-      <h3 className={headingStyle}>Capabilities</h3>
-      <p>{phase.capabilities}</p>
+          <h3 className={headingStyle}>Capabilities</h3>
+          <p>{phase.capabilities}</p>
 
-      <h3 className={headingStyle}>Life</h3>
-      <p>{phase.life}</p>
+          <h3 className={headingStyle}>Life</h3>
+          <p>{phase.life}</p>
 
-      <h3 className={headingStyle}>Money</h3>
-      <p>{phase.money}</p>
+          <h3 className={headingStyle}>Money</h3>
+          <p>{phase.money}</p>
 
-      <h3 className={headingStyle}>Culture</h3>
-      <p>{phase.culture}</p> */}
+          <h3 className={headingStyle}>Culture</h3>
+          <p>{phase.culture}</p>
+        </>
+      )}
     </div>
   );
 };
