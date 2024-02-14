24eb4d57c14be46803b09d324421ae07a0a90f12
Jacob Sansbury <sansburyjacob@gmail.com>
2024-02-13 16:26:12 -0800

    Fxies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { ScrollAdapter } from "./ScrollAdapter";
 
 export default function Home() {
   return (
-    <main className="w-fit">
+    <main className="">
       <ScrollAdapter />
 
       <div className="grid grid-cols-3 h-[100svh] p-3 gap-3 w-[280vw] md:w-screen min-w-[1500px]">
